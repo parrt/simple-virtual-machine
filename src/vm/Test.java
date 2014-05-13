4ea3200e7b6ff9f742c75a8f1318319b49de4cd3
Terence Parr <parrt@antlr.org>
2014-05-13 09:53:13 -0700

    back out to simple vm w/o functions

--- src/vm/Test.java
+++ src/vm/Test.java
@@ -2,18 +2,13 @@ package vm;
 
 import static vm.Bytecode.BR;
 import static vm.Bytecode.BRF;
-import static vm.Bytecode.CALL;
 import static vm.Bytecode.GLOAD;
 import static vm.Bytecode.GSTORE;
 import static vm.Bytecode.HALT;
 import static vm.Bytecode.IADD;
 import static vm.Bytecode.ICONST;
 import static vm.Bytecode.ILT;
-import static vm.Bytecode.IMUL;
-import static vm.Bytecode.ISUB;
-import static vm.Bytecode.LOAD;
 import static vm.Bytecode.PRINT;
-import static vm.Bytecode.RET;
 
 public class Test {
 	static int[] hello = {
@@ -47,41 +42,11 @@ public class Test {
 			HALT					// 24
 	};
 
-	static int[] factorial = {
-//.def fact: ARGS=1, LOCALS=0		ADDRESS
-//	IF N < 2 RETURN 1
-			LOAD, -3,				// 0
-			ICONST, 2,				// 2
-			ILT,					// 4
-			BRF, 10,				// 5
-			ICONST, 1,				// 7
-			RET,					// 9
-//CONT:
-//	RETURN N * FACT(N-1)
-			LOAD, -3,				// 10
-			LOAD, -3,				// 12
-			ICONST, 1,				// 14
-			ISUB,					// 16
-			CALL, 0, 1,				// 17
-			IMUL,					// 20
-			RET,					// 21
-//.DEF MAIN: ARGS=0, LOCALS=0
-// PRINT FACT(10)
-			ICONST, 5,				// 22
-			CALL, 0, 1,				// 24
-			PRINT,					// 27
-			HALT					// 28
-	};
-
 	public static void main(String[] args) {
-//		VM vm = new VM(hello, 0, 0);
-//		vm.exec();
-//
-//		vm = new VM(loop, 0, 2);
-//		vm.trace = true;
-//		vm.exec();
+		VM vm = new VM(hello, 0, 0);
+		vm.exec();
 
-		VM vm = new VM(factorial, 22, 0);
+		vm = new VM(loop, 0, 2);
 		vm.trace = true;
 		vm.exec();
 	}
